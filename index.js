081c08ef927203b8c1d9e8d29a5a20a1d8a412d0
felix mwangi <fmwauramwangi@gmail.com>
2024-11-27 13:48:44 +0300

    index

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 1000) + 1;
+console.log(random);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(5, 10, 15, 20);
+console.log(max);
